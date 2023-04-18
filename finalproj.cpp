89c5ca0e7e32d8d0f612887e812985899186c98c
Faye J. Stover <113383138+fayejstover@users.noreply.github.com>
2023-04-18 12:54:19 -0400

    Create finalproj.cpp

---
+++ finalproj.cpp
@@ -0,0 +1,27 @@
+# Your names: Faye Stover, Agnes Mar, Chloe Emch
+
+import matplotlib.pyplot as plt
+import os
+import sqlite3
+import unittest
+
+# implement functions here:
+
+
+
+
+# main:
+def main():
+
+
+
+
+# test cases: 
+class TestHW8(unittest.TestCase):
+
+
+
+        
+if __name__ == '__main__':
+    main()
+    unittest.main(verbosity=2)
