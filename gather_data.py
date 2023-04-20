9d0c2fc16c332316cf594852a4f8936cd3c6df67
Faye J. Stover <113383138+fayejstover@users.noreply.github.com>
2023-04-19 20:43:44 -0400

    more

--- gather_data.py
+++ gather_data.py
@@ -1,3 +1,6 @@
+# SI 206 FINAL PROJECT
+# AGNES MAR, FAYE STOVER, CHLOE EMCH
+
 import matplotlib.pyplot as plt
 import os
 import sqlite3
@@ -9,18 +12,18 @@ import json
 # implement functions here:
 
 ############## RICK AND MORTY API #################################################################################################
-
 def open_RM_database(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
-    cur = conn.cursor()
-    return cur, conn
+    return conn
+
 
 def read_RM_api(url):
     req = requests.get(url)
     info = req.text
     text = json.loads(info)
     return text
+
 
 def insert_RM_data():
     conn = sqlite3.connect('finalproj.db')
@@ -45,21 +48,22 @@ def insert_RM_data():
 
     conn.commit()
     conn.close()
+
     
 
 
 ############## POKEMON API ########################################################################################################
-def open_database(db_name):
+def open_POKEMON_database(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
-    cur = conn.cursor()
-    return cur, conn
+    return conn
 
-def read_api(url):
+def read_POKEMON_api(url):
     req = requests.get(url)
     info = req.text
     text = json.loads(info)
     return text
+
 
 def pokemon_data():
     conn = sqlite3.connect('finalproj.db')
@@ -67,25 +71,27 @@ def pokemon_data():
     c.execute('CREATE TABLE IF NOT EXISTS Pokemon (name TEXT, id INTEGER, height INTEGER, weight INTEGER)')
     conn.commit()
 
-    data_dic = read_api('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+    data_dic = read_POKEMON_api('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
     result = c.execute('SELECT COUNT(*) FROM Pokemon')
     db_length = result.fetchone()[0]
     
-    for x in range(db_length, db_length + 25):
-        name = data_dic['results'][x]['name']
-        url = data_dic['results'][x]['url']
-        pokemon_api = read_api(url)
-        height = pokemon_api['height']
-        id = pokemon_api['id']
-        weight = pokemon_api['weight']
-        c.execute("INSERT OR IGNORE INTO Pokemon (name, id, height, weight) VALUES (?,?,?,?)",(name, id, height, weight))
+    for x in range(db_length, db_length + 100, 25):
+        data_slice = data_dic['results'][x:min(x+25, db_length+100)]
+        for pokemon in data_slice:
+            name = pokemon['name']
+            url = pokemon['url']
+            pokemon_api = read_POKEMON_api(url)
+            height = pokemon_api['height']
+            id = pokemon_api['id']
+            weight = pokemon_api['weight']
+            c.execute("INSERT OR IGNORE INTO Pokemon (name, id, height, weight) VALUES (?,?,?,?)",(name, id, height, weight))
     conn.commit()
     conn.close()
 
     
 
 ########## HARRY POTTER API ####################################################################################################
-def get_hp_data():
+def get_HP_data():
     url = "https://hp-api.onrender.com/api/characters"
     response = requests.get(url)
 
@@ -96,7 +102,7 @@ def get_hp_data():
         print("error (1)")
 
 
-def extract_hp_data(data, start, end):
+def extract_HP_data(data, start, end):
     characters = []
     for i, character in enumerate(data):
         if i < start:
@@ -113,8 +119,7 @@ def extract_hp_data(data, start, end):
     return characters
 
 
-
-def create_hp_table():
+def create_HP_table():
     conn = sqlite3.connect("finalproj.db")
     c = conn.cursor()
 
@@ -132,12 +137,12 @@ def create_hp_table():
     conn.close()
 
 
-def insert_hp_data(start=0, end=100):
+def insert_HP_data(start=0, end=100):
     conn = sqlite3.connect("finalproj.db")
     c = conn.cursor()
 
-    data = get_hp_data()
-    characters = extract_hp_data(data, start, end)
+    data = get_HP_data()
+    characters = extract_HP_data(data, start, end)
 
     c.executemany("INSERT INTO HarryPotterCharacters VALUES (?, ?, ?, ?)", characters)
 
@@ -146,11 +151,10 @@ def insert_hp_data(start=0, end=100):
 
     
     
- 
 ################# NBA API ##########################################################################################################
 ############## EXTRA CREDIT ########################################################################################################
-'''
-def get_nba_data():
+
+def get_NBA_data():
     url = "https://stats.nba.com/stats/leagueleaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=2021-22&SeasonType=Regular+Season&StatCategory=PTS"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
@@ -161,26 +165,33 @@ def get_nba_data():
         data = response.json()
         return data
     else:
-        print("error (1)")
+        print("error (2)")
 
-def extract_nba_data(data):
+def extract_NBA_data(data):
     players = []
     for player in data["resultSet"]["rowSet"]:
         try:
             name = str(player[2])
             team = str(player[4])
-            ft_pct = str(player[15])
             pts = str(player[23])
-            eff = str(player[24])
-            
-            row = (name, team, ft_pct, pts, eff)
+            reb = str(player[18])
+            ast = str(player[19])
+            stl = str(player[20])
+            blk = str(player[21])
+            fgm = str(player[11])
+            fga = str(player[12])
+            ftm = str(player[14])
+            fta = str(player[15])
+            tov = str(player[22])
+            gp = str(player[6])
+
+            row = (name, team, pts, reb, ast, stl, blk, fgm, fga, ftm, fta, tov, gp)
             players.append(row)
         except IndexError:
             pass
     return players
 
-
-def create_nba_table():
+def create_NBA_table():
     conn = sqlite3.connect("finalproj.db")
     c = conn.cursor()
 
@@ -189,48 +200,52 @@ def create_nba_table():
         CREATE TABLE NBAplayers (
             PLAYER TEXT,
             TEAM TEXT,
-            FT_PCT TEXT,
             PTS TEXT,
-            EFF TEXT
+            REB TEXT,
+            AST TEXT,
+            STL TEXT,
+            BLK TEXT,
+            FGM TEXT,
+            FGA TEXT,
+            FTM TEXT,
+            FTA TEXT,
+            TOV TEXT,
+            GP TEXT
         )
     """)
 
     conn.commit()
     conn.close()
 
-def insert_nba_data(players):
+def insert_NBA_data(players):
     conn = sqlite3.connect("finalproj.db")
     c = conn.cursor()
-    c.executemany("INSERT INTO NBAplayers VALUES (?, ?, ?, ?, ?)", players)
+    c.executemany("INSERT INTO NBAplayers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", players)
 
     conn.commit()
     conn.close()
-'''
+
 
 ################# MAIN ##########################################################################################################
 
 def main():
     
     # calls from RICK AND MORTY
-    cur, conn = open_RM_database('finalproj.db')
+    rm_conn = open_RM_database('finalproj.db')
     insert_RM_data()
-    conn.close()
+    rm_conn.close()
     
     # calls from POKEMON
-    open_database('Pokemon')
-    read_api('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+    poke_conn = open_POKEMON_database('finalproj.db')
     pokemon_data()
+    poke_conn.close()
     
     # calls from HARRY POTTER
-    create_hp_table()
-    hp_data = get_hp_data()
-    hp_characters = extract_hp_data(hp_data, start=0, end=100)  # Provide start and end arguments
-    insert_hp_data()
-    pokemon_data()
-    insert_RM_data()
+    create_HP_table()
+    hp_data = get_HP_data()
+    extract_HP_data(hp_data, start=0, end=100)  
+    insert_HP_data()
 
-    
-    # retrieve the data from the database
     conn = sqlite3.connect("finalproj.db")
     c = conn.cursor()
     c.execute("SELECT * FROM HarryPotterCharacters")
@@ -238,11 +253,13 @@ def main():
     conn.close()
 
     # calls from NBA
-    #q_data = get_nba_data()
-    #quotes = extract_nba_data(q_data)
-    #create_nba_table()
-    #insert_nba_data(quotes)
+    data = get_NBA_data()
+    players = extract_NBA_data(data)
+    create_NBA_table()
+    insert_NBA_data(players)
 
 
 if __name__ == "__main__":
     main()
+
+
